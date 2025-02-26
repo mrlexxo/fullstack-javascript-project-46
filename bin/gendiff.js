2caec50bda72a006cfee41f3944c283bd6597ede
mrlexxo <alexander.ogorodov@gmail.com>
2025-02-26 16:08:29 +0400

    s2: gendif help correction

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,9 @@ const program = new Command();
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
+    .usage('[options] <filepath1> <filepath2>')
     .option('-v, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format')
     .option('-h, --help', 'output usage information')
     .parse(process.argv);
 
