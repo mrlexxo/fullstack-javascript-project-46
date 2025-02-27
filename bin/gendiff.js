41e2661d6bdb0441344e3f192fee30994043322e
mrlexxo <alexander.ogorodov@gmail.com>
2025-02-27 14:08:34 +0400

    s3: gendiff logic implemented, src/index.js gendiff implemented, parser.js created not yet implemented

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,14 @@ program
     .option('-v, --version', 'output the version number')
     .option('-f, --format [type]', 'output format')
     .option('-h, --help', 'output usage information')
-    .parse(process.argv);
+    .argument('<filePath1>')
+    .argument('<filePath2>')
+    .action((filePath1, filePath2) => {
+        const options = program.opts().format;
+        const result = gendiff(filePath1, filePath2, options);
+        console.log(result);
+      })
+    .parse(process.argv)
 
 if (program.opts().help) {
     program.help();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const gendiff = () => {
-    console.log('Link established');
-};
+import { readFileSync } from 'fs';
+import path from 'node:path';
+import process from 'node:process';
+import parse from './parser.js';
+import formatData from './formatters/index.js'
+
+const getFileType = (filePath) => path.extname(filePath).slice(1);
+const getFilePath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => readFileSync(getFilePath(filePath), 'utf-8');
+const parseData = (filePath, extension) => parse(filePath, extension);
+
+
+
+const gendiff = (filePath1, filePath2, format = 'stylish') => {
+    const extention1 = getFileType(filePath1);
+    const path1 = getFilePath(filePath1);
+    const data1 = readFile(path1);
+    const parsedData1 = parseData(data1, extention1);
+
+    const extention2 = getFileType(filePath2);
+    const path2 = getFilePath(filePath2);
+    const data2 = readFile(path2);
+    const parsedData2 = parseData(data2, extention2);
+
+    const diff = generateDiff(parsedData1, parsedData2);
+    return formatData(diff, format);
+}
 
 export default gendiff;
